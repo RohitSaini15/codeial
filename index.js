915724826b219691948a3526b55204ac9a994261
Rohit Saini <r7665415553@gmail.com>
2022-10-14 18:04:54 +0530

    add routing,controller

--- index.js
+++ index.js
@@ -3,15 +3,12 @@ const app=express();
 
 const port=8000;
 
-
-
-
+app.use('/',require('./routes'))
 
 app.listen(port,(err)=>{
     if(err){
         console.log(`Error in running the server: ${err}`);
         return;
     }
-
     console.log(`Server is running at port: ${port}`);
 })
