9899092525798ee55e0dec015c7d64624b1fd203
Rohit saini <r7665415553@gmail.com>
2022-10-17 12:43:27 +0530

    add mongodb

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express=require('express')
 const app=express();
 
 const expressLayouts=require('express-ejs-layouts')
+require('./config/mongoose')
 
 const port=8000;
 
+app.use(express.static("./assets"))
 app.use(expressLayouts)
+
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
 
 app.use('/',require('./routes'))
 
